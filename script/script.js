0dec4a19be9f19dcb1b5edc562d37900ec1337ee
tatyana <tilkka@mail.ru>
2023-06-05 18:07:44 +0300

    feat: tasks from lesson 2

--- script/script.js
+++ script/script.js
@@ -201,127 +201,128 @@
 // Задание 1
 // Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
 
-const numbers = {
-keyin1: 1,
-keyin2: 2,
-keyin3: 3,
-keyin4: 4,
-keyin5: 5,
-keyin6: 6,
-keyin7: 7,
-}
+// const numbers = {
+// keyin1: 1,
+// keyin2: 2,
+// keyin3: 3,
+// keyin4: 4,
+// keyin5: 5,
+// keyin6: 6,
+// keyin7: 7,
+// }
 
-const numbersValues = Object.values(numbers).filter(element => element >= 3);
-console.log(numbersValues);
+// const numbersValues = Object.values(numbers).filter(element => element >= 3);
+// console.log(numbersValues);
 
-// Задание 2
-// Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
+// // Задание 2
+// // Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
 
-const post = {
-    author: "John", // вывести этот текст
-    postId: 23,
-    comments: [
-        {
-            userId: 10,
-            userName: "Alex",
-            text: "lorem ipsum",
-            rating: {
-                likes: 10,
-                dislikes: 2, // вывести это число
-                },
-        },
-        {
-            userId: 5, // вывести это число
-            userName: "Jane",
-            text: "lorem ipsum 2", // вывести этот текст
-            rating: {
-                likes: 3,
-                dislikes: 1,
-            },
-        },
-    ],
-};
+// const post = {
+//     author: "John", // вывести этот текст
+//     postId: 23,
+//     comments: [
+//         {
+//             userId: 10,
+//             userName: "Alex",
+//             text: "lorem ipsum",
+//             rating: {
+//                 likes: 10,
+//                 dislikes: 2, // вывести это число
+//                 },
+//         },
+//         {
+//             userId: 5, // вывести это число
+//             userName: "Jane",
+//             text: "lorem ipsum 2", // вывести этот текст
+//             rating: {
+//                 likes: 3,
+//                 dislikes: 1,
+//             },
+//         },
+//     ],
+// };
 
-const array = [];
-array.push(post.author, post.comments[0].rating.dislikes, post.comments[1].userId, post.comments[1].text);
-console.log(array);
+// const array = [];
+// array.push(post.author, post.comments[0].rating.dislikes, post.comments[1].userId, post.comments[1].text);
+// console.log(array);
 
-// Задание 3
-// Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+// // Задание 3
+// // Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
 
-const products = [
-    {
-        id: 3,
-        price: 200,
-    },
-    {
-        id: 4,
-        price: 900,
-    },
-    {
-        id: 1,
-        price: 1000,
-    },
-];
+// const products = [
+//     {
+//         id: 3,
+//         price: 200,
+//     },
+//     {
+//         id: 4,
+//         price: 900,
+//     },
+//     {
+//         id: 1,
+//         price: 1000,
+//     },
+// ];
     
-products.forEach(elem => elem.price = elem.price - elem.price * 0.15);
-console.log(products);
+// products.forEach(elem => elem.price = elem.price - elem.price * 0.15);
+// console.log(products);
 
-// Задание 4
-// 1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив productsNew.
-// 2. Необходимо отсортировать массив productsNew используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+// // Задание 4
+// // 1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив productsNew.
+// // 2. Необходимо отсортировать массив productsNew используя метод sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
 
 
-const productsNew = [
-    {
-        id: 3,
-        price: 127,
-        photos: [
-            "1.jpg",
-            "2.jpg",
-        ],
-    },
-    {
-        id: 5,
-        price: 499,
-        photos: [],
-    },
-    {
-        id: 10,
-        price: 26,
-        photos: [
-        "3.jpg",
-        ],
-    },
-    {
-        id: 8,
-        price: 78,
-    },
-];
+// const productsNew = [
+//     {
+//         id: 3,
+//         price: 127,
+//         photos: [
+//             "1.jpg",
+//             "2.jpg",
+//         ],
+//     },
+//     {
+//         id: 5,
+//         price: 499,
+//         photos: [],
+//     },
+//     {
+//         id: 10,
+//         price: 26,
+//         photos: [
+//         "3.jpg",
+//         ],
+//     },
+//     {
+//         id: 8,
+//         price: 78,
+//     },
+// ];
 
-const photosArray = productsNew.filter(elem => elem.photos && elem.photos != 0);
-console.log(photosArray);
+// const photosArray = productsNew.filter(elem => elem.photos && elem.photos != 0);
+// console.log(photosArray);
 
-// const sortArray = productsNew.sort((a, b) => a.price > b.price ? 1 : -1);
-const sortArray = productsNew.sort((a, b) => a.price - b.price);
-console.log(sortArray);
+// // const sortArray = productsNew.sort((a, b) => a.price > b.price ? 1 : -1);
+// const sortArray = productsNew.sort((a, b) => a.price - b.price);
+// console.log(sortArray);
 
-// **Задание 5**
-// Дано 2 массива 
-// Вам необходимо объединить 2 этих массива, чтобы значения первого массива были ключами, а значения второго массива — значениями.
+// // **Задание 5**
+// // Дано 2 массива 
+// // Вам необходимо объединить 2 этих массива, чтобы значения первого массива были ключами, а значения второго массива — значениями.
 
-const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+// const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
-const weekObj = {};
+// const weekObj = {};
 
-if (en.length === ru.length) {
-    for (let i = 0; i < en.length; i++) {
-        weekObj[en[i]] = ru[i];
-    }
-}
-console.log(weekObj);
+// if (en.length === ru.length) {
+//     for (let i = 0; i < en.length; i++) {
+//         weekObj[en[i]] = ru[i];
+//     }
+// }
+// console.log(weekObj);
 
-// Проговорю этот вариант: в коллбэк функцию подаем три параметра - наше конечное значение (аккумулятор), которое будет объектом (это видно в конце строки после запятой), элемент массива en, по которому мы бежим и индекс, чтобы обращаться ко второму массиву. Далее мы присваиваем ключ и значение и возвращаем объект. Все тоже самое происходит и со вторым элементом массива. В конечном итоге на выходе у нас будет набран объект.
-const reseultOfReduce = en.reduce((acc, elem, index) => (acc[elem] = ru[index], acc), {});
-console.log(reseultOfReduce);
+// // Проговорю этот вариант: в коллбэк функцию подаем три параметра - наше конечное значение (аккумулятор), которое будет объектом (это видно в конце строки после запятой), элемент массива en, по которому мы бежим и индекс, чтобы обращаться ко второму массиву. Далее мы присваиваем ключ и значение и возвращаем объект. Все тоже самое происходит и со вторым элементом массива. В конечном итоге на выходе у нас будет набран объект.
+// const reseultOfReduce = en.reduce((acc, elem, index) => (acc[elem] = ru[index], acc), {});
+// console.log(reseultOfReduce);
+
