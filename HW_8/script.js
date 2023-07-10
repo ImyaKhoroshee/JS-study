a729b286542d6698ee1269ffc5350977214f79cc
tatyana <tilkka@mail.ru>
2023-07-10 18:14:00 +0300

    feat: star task from last hw

--- HW_8/script.js
+++ HW_8/script.js
@@ -75,3 +75,100 @@ order.addProduct(product2);
 
 console.log(`Cписок покупок: ${order.listOfProducts.map(el => el.name)}`);
 console.log(`Общая стоимость заказа ${order.getTotalPrice()} рублей`);
+
+// // ****** Задача необязательная для выполнения:
+// // Это расширенная версия задачи с банком, которую мы решлали на семинаре:
+// // Создайте класс "Банк", который будет иметь следующие свойства: название банка, список клиентов и список счетов. Класс должен иметь методы для добавления нового клиента, открытия нового счета для клиента, пополнения счета, снятия денег со счета и проверки баланса.
+
+class Bank {
+    
+    listOfClient = [];
+    listOfAccounts = [];
+
+    constructor(bankTitle) {
+        this.bankTitle = bankTitle;
+    }
+    addClient(client) {
+        this.listOfClient.push({clientId: client.id, nameClient: client.name, ageClient: client.age});
+    }
+    openAccount(client, account) {
+        this.listOfAccounts.push({
+            accountId: client.id,
+            accountNumber: account.accountNumber,
+            accountBalance: account.balance
+        });
+    }
+    deposit(clientAccount, amountToDeposit) {
+        this.listOfAccounts.forEach(account => {
+            if (account.accountId === clientAccount) {
+                account.accountBalance += amountToDeposit;
+                console.log(`Баланс счета №${account.accountId} после пополнения: ${account.accountBalance}`);
+            }
+        });
+        return this.listOfAccounts;
+    }
+    withdraw(clientAccount, amountTowithdraw) {
+        this.listOfAccounts.forEach(account => {
+            if (account.accountId === clientAccount) {
+                if (account.accountBalance > amountTowithdraw) {
+                    account.accountBalance -= amountTowithdraw;
+                    console.log(`Баланс счета №${account.accountId} после списания: ${account.accountBalance}`);
+                } else {
+                    console.log(`Недостаточно средств для снятия`);
+                }
+            }
+        });
+        return this.listOfAccounts;
+    }
+    checkBalance(clientAccount) {
+        this.listOfAccounts.forEach(account => {
+            if (account.accountNumber === clientAccount) {
+                console.log(account.accountBalance);
+            }
+        });
+    }
+}
+
+class Client {
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Account extends Client {
+
+    constructor(client, accountNumber, balance) {
+        super(client.id);
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
+
+const bank = new Bank("Мой Банк");
+
+const client1 = new Client(1, "Иван", 25);
+const client2 = new Client(2, "Мария", 30);
+
+const account1 = new Account(client1, 223344, 5000);
+const account2 = new Account(client2, 556677, 1000);
+
+bank.addClient(client1);
+bank.addClient(client2);
+
+bank.openAccount(client1, account1);
+bank.openAccount(client2, account2);
+
+console.log(bank.listOfClient);
+console.log(bank.listOfAccounts);
+
+bank.deposit(1, 1000);
+bank.deposit(2, 1000);
+
+bank.withdraw(1, 500);
+bank.withdraw(2, 500);
+
+bank.checkBalance(223344);
+bank.checkBalance(556677);
+
