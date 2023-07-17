4a4be225b1ab870299751769227e49ed45e6e76c
tatyana <tilkka@mail.ru>
2023-07-17 15:10:06 +0300

    fix: refuse comments

--- HW_9/data.js
+++ HW_9/data.js
@@ -1,93 +1,93 @@
-// // Шпоргалка
-// // setItem() - Добавить ключ / значение в LocalStorage
-// // getItem() - Получить значение из LocalStorage
-// // removeItem() - Удалить значение по его ключу
-// // clear() - Удалить все элементы из LocalStorage
-// // key() - Получить ключ предмета из LocalStorage
+// Шпоргалка
+// setItem() - Добавить ключ / значение в LocalStorage
+// getItem() - Получить значение из LocalStorage
+// removeItem() - Удалить значение по его ключу
+// clear() - Удалить все элементы из LocalStorage
+// key() - Получить ключ предмета из LocalStorage
 
-// // Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users) и отобразить их на странице. Пользователь должен иметь возможность удалить любого пользователя из списка. Данные при получении необходимо сохранить в локальное хранилище браузера localStorage. При удалении пользователь должен удаляться не только со страницы, но и из локального хранилища localStorage
+// Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users) и отобразить их на странице. Пользователь должен иметь возможность удалить любого пользователя из списка. Данные при получении необходимо сохранить в локальное хранилище браузера localStorage. При удалении пользователь должен удаляться не только со страницы, но и из локального хранилища localStorage
 
-// const getData = async (url) => {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     data.forEach(item => { localStorage.setItem(JSON.stringify(item.id), JSON.stringify(item)) });
-// }
+const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    data.forEach(item => { localStorage.setItem(JSON.stringify(item.id), JSON.stringify(item)) });
+}
 
-// getData('https://jsonplaceholder.typicode.com/users').catch((e) => console.log(`Что-то пошло не так! ${e}`));
+getData('https://jsonplaceholder.typicode.com/users').catch((e) => console.log(`Что-то пошло не так! ${e}`));
 
-// // Отрисовка из LocalStorage
-// const body = document.querySelector('.body');
+// Отрисовка из LocalStorage
+const body = document.querySelector('.body');
 
-// for (let i = 1; i <= localStorage.length; i++) {
+for (let i = 1; i <= localStorage.length; i++) {
 
-//     const itemOfLocalStorage = JSON.parse(localStorage.getItem(i));
+    const itemOfLocalStorage = JSON.parse(localStorage.getItem(i));
 
-//     const userCard = document.createElement('div');
-//     userCard.classList.add('card');
-//     userCard.dataset.id = itemOfLocalStorage.id;
-//     body.append(userCard);
+    const userCard = document.createElement('div');
+    userCard.classList.add('card');
+    userCard.dataset.id = itemOfLocalStorage.id;
+    body.append(userCard);
 
-//     const imageTop = document.createElement('img');
-//     imageTop.src = 'top.jpg';
+    const imageTop = document.createElement('img');
+    imageTop.src = 'top.jpg';
     
-//     const imageBottom = document.createElement('img');
-//     imageBottom.src = 'bottom.jpg';
+    const imageBottom = document.createElement('img');
+    imageBottom.src = 'bottom.jpg';
 
-//     const cardContent = document.createElement('div');
-//     cardContent.classList.add('card__content');
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card__content');
     
 
-//     userCard.append(imageTop, cardContent, imageBottom);
+    userCard.append(imageTop, cardContent, imageBottom);
 
-//     const userName = document.createElement('h1');
-//     userName.classList.add('card__user-name');
-//     userName.textContent = itemOfLocalStorage.name;
+    const userName = document.createElement('h1');
+    userName.classList.add('card__user-name');
+    userName.textContent = itemOfLocalStorage.name;
 
-//     const userCompany = document.createElement('p');
-//     userCompany.classList.add('card__user-company');
-//     userCompany.textContent =  itemOfLocalStorage.company.name;
+    const userCompany = document.createElement('p');
+    userCompany.classList.add('card__user-company');
+    userCompany.textContent =  itemOfLocalStorage.company.name;
 
-//     const userInfoList = document.createElement('ul');
-//     userInfoList.classList.add('card__user-infolist');
+    const userInfoList = document.createElement('ul');
+    userInfoList.classList.add('card__user-infolist');
 
-//     const deleteButton = document.createElement('button');
-//     deleteButton.classList.add('card__button');
-//     deleteButton.id = itemOfLocalStorage.id;
-//     deleteButton.textContent = 'Delete';
-
-
-//     cardContent.append(userName, userCompany, userInfoList, deleteButton);
-
-//     const userInfoItemFirstChild = document.createElement('li');
-//     userInfoItemFirstChild.classList.add('card__user-infoitem');
-//     userInfoItemFirstChild.textContent = 'Username: ' + itemOfLocalStorage.username;
-
-//     const userInfoItemSecondChild = document.createElement('li');
-//     userInfoItemSecondChild.classList.add('card__user-infoitem');
-//     userInfoItemSecondChild.textContent = 'Email: ' + itemOfLocalStorage.email;
-
-//     const userInfoItemThirdChild = document.createElement('li');
-//     userInfoItemThirdChild.classList.add('card__user-infoitem');
-//     userInfoItemThirdChild.textContent = 'Phone: ' + itemOfLocalStorage.phone;
-
-//     const userInfoItemFourthChild = document.createElement('li');
-//     userInfoItemFourthChild.classList.add('card__user-infoitem');
-//     userInfoItemFourthChild.textContent = 'Website: ' + itemOfLocalStorage.website;
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('card__button');
+    deleteButton.id = itemOfLocalStorage.id;
+    deleteButton.textContent = 'Delete';
 
 
-//     userInfoList.append(userInfoItemFirstChild, userInfoItemSecondChild, userInfoItemThirdChild, userInfoItemFourthChild);
+    cardContent.append(userName, userCompany, userInfoList, deleteButton);
+
+    const userInfoItemFirstChild = document.createElement('li');
+    userInfoItemFirstChild.classList.add('card__user-infoitem');
+    userInfoItemFirstChild.textContent = 'Username: ' + itemOfLocalStorage.username;
+
+    const userInfoItemSecondChild = document.createElement('li');
+    userInfoItemSecondChild.classList.add('card__user-infoitem');
+    userInfoItemSecondChild.textContent = 'Email: ' + itemOfLocalStorage.email;
+
+    const userInfoItemThirdChild = document.createElement('li');
+    userInfoItemThirdChild.classList.add('card__user-infoitem');
+    userInfoItemThirdChild.textContent = 'Phone: ' + itemOfLocalStorage.phone;
+
+    const userInfoItemFourthChild = document.createElement('li');
+    userInfoItemFourthChild.classList.add('card__user-infoitem');
+    userInfoItemFourthChild.textContent = 'Website: ' + itemOfLocalStorage.website;
+
+
+    userInfoList.append(userInfoItemFirstChild, userInfoItemSecondChild, userInfoItemThirdChild, userInfoItemFourthChild);
     
-// }
+}
 
-// // // Удаление карточки с экрана по клику на кнопку, а также из LocalStorage
-// // const deleteButtons = document.querySelectorAll('.card__button');
+// // Удаление карточки с экрана по клику на кнопку, а также из LocalStorage
+// const deleteButtons = document.querySelectorAll('.card__button');
 
-// deleteButtons.forEach(button => {button.addEventListener('click', function (e) {
+deleteButtons.forEach(button => {button.addEventListener('click', function (e) {
         
-//     e.target.closest(`[data-id='${e.target.id}']`).remove();
-//     localStorage.removeItem(e.target.id);
-//     }
-// )});
+    e.target.closest(`[data-id='${e.target.id}']`).remove();
+    localStorage.removeItem(e.target.id);
+    }
+)});
 
 
 
